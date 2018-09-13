8cfee50b704d96333b245ffbc8cba60413680339
Nohabroha <noahysimon@gmail.com>
2018-09-12 21:24:11 -0500

    testing

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
-// Set up mySQL connection
-var mysql = require("mysql");
+const mysql = require("mysql");
+
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password:"1234",
-        database: "burgers_db"
-    });
+   connection = mysql.createConnection({
+       host: "localhost",
+       user: "root",
+       password: "1234",
+       database: "burgers_db"
+   });
 };
 
-// Make connection
 connection.connect();
 module.exports = connection;
